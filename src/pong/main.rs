d6f48537890d894c7196f7ad64859a810666da9f
Torste Aikio <zokier@gmail.com>
2014-01-02 12:52:14 +0200

    Updated copyright information

--- src/pong/main.rs
+++ src/pong/main.rs
@@ -1,4 +1,5 @@
-// Copyright 2013 Torste Aikio and gl-rs developers. 
+// Copyright 2013-2014 Torste Aikio and others. 
+// See AUTHORS file for details. 
 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
